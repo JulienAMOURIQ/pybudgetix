d05217d07d7bd0796d4a80499cec45ecd548a516
Julien AMOURIQ <julien.amouriq@etu.univ-tours.fr>
2018-09-13 22:55:05 +0200

    ajout de la structure des tests unitaires et d'une première version du modèle de données

---
+++ src/tests/test_budget.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+module test
+"""
+
+from goupilsoft import budget
+
+def test_cree_base():
+    """
+        Un premier test
+    """
+    import sys
+    print(sys.version)
+    assert 1 == 1
+    
